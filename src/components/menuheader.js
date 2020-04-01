ca4e5b169d807a191cabb015e197765047712c50
jyotics <jyotics09@gmail.com>
2020-04-01 17:10:37 +0530

    Logo

--- src/components/menuheader.js
+++ src/components/menuheader.js
@@ -25,10 +25,11 @@ export default  class MenuHeader extends Component {
           <h2><i className="fa fa-bug" aria-hidden="true"></i> COVID-19</h2>
 
           <a className="navbar-brand" href="#"><i className="color_g">Last Updated At : </i><Moment format="dddd, MMMM Do YYYY hh:mm a" tz="Asia/Kolkata">{this.props.updated_since }</Moment></a>
+          
 
 
 
-    </nav></div>;
+    </nav><p className="pull-right" style={{"padding":"2px"}}>Auto Refreshed after 5 minutes</p></div>;
 
     }
   }
