cddb1de0275caafdf8195315606a55a18bb6ecdf
deepaksmvdu <dkmail.37@gmail.com>
2020-04-03 21:23:08 +0530

    Adding footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
 export default  class Footer extends Component {
     render() {
-      return <footer className="page-footer font-small special-color-dark pt-4"><div class="container">
-        <div className="footer-copyright text-center" style={{"background-color": "#ddd"}}>© 2020 Copyright:
-          <a href="#">LivesUpdates</a></div>
-        </div>
+      return <footer className="page-footer font-small cyan darken-3">
+                  <div class="row">
+                      <div class="col-md-12 py-5">
+                          <div class="mb-5 flex-center">
+
+                              <a class="">
+                                  <i class="fab fa-lg white-text mr-md-5 mr-3 fa-2x">Livesupdates</i>
+                              </a>
+                          </div>
+                      </div>
+
+
+                  </div>
+
+              <div class="footer-copyright text-center py-3">© 2020 Copyright:
+                  <a href="https://livesupdates.com/"> livesupdates.com</a>
+              </div>
       
       </footer>
       
